0b434a0ced141350503f0ac20fbe15d8edf2a2b1
tc <tc@tizilogic.com>
2019-06-21 03:38:14 +0200

    Disabled SSL certificate check and added Status Label
    urllib.request.urlopen is unable to verify CA with letsencrypt on
    Android. Needs further investigation.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,41 @@
+"""
+Client application using Kivy.
+"""
+
+__author__ = 'Tiziano Bettio'
+__license__ = 'MIT'
+__version__ = '0.1'
+__copyright__ = """Copyright (c) 2019 Tiziano Bettio
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE."""
+
 import hashlib
 import os
 import time
+import ssl
 import urllib.request as request
 import urllib.error
 
 import kivy
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.settings import SettingsWithTabbedPanel
 from kivy.core.window import Window
@@ -20,8 +49,16 @@ kivy.require('1.11.0')
 Builder.load_string('''
 <Interface>:
     orientation: 'vertical'
+    Label:
+        id: status_label
+        size_hint: 1.0, 0.045
+        text: app.status_text
+        font_size: '14sp'
+        bold: True
+        italic: True
+        background_color: [0.15, 0.45, 0.1, 1]
     Button:
-        size_hint: 1.0, 0.9
+        size_hint: 1.0, 0.85
         text: 'Open Sesame!'
         font_size: '32sp'
         on_release: app.open_door()
@@ -43,15 +80,24 @@ class Interface(BoxLayout):
 
 
 class GDOClient(App):
+    status_text = StringProperty()
+
     def open_door(self):
+        self.status_text = 'State: trying to open...'
         https = int(self.config.get('Connection', 'https'))
         protocol = 'https' if https else 'http'
         address = self.config.get('Connection', 'address')
         port = self.config.get('Connection', 'port')
         base_url = f'{protocol}://{address}:{port}'
         try:
-            salt = request.urlopen(f'{base_url}/salt').read().decode()
+            kw = {'url': f'{base_url}/salt'}
+            if kw['url'].startswith('https'):
+                ctx = ssl.SSLContext()
+                ctx.verify_mode = ssl.CERT_NONE
+                kw['context'] = ctx
+            salt = request.urlopen(**kw).read().decode()
         except urllib.error.URLError:
+            self.status_text = 'State: failed to get salt'
             return False
 
         hm = hashlib.sha3_512()
@@ -64,11 +110,22 @@ class GDOClient(App):
         url = f'{base_url}/open?{salt}={hashstring}'
 
         try:
-            result = request.urlopen(url).read().decode()
+            kw = {'url': url}
+            if kw['url'].startswith('https'):
+                ctx = ssl.SSLContext()
+                ctx.verify_mode = ssl.CERT_NONE
+                kw['context'] = ctx
+            result = request.urlopen(**kw).read().decode()
         except urllib.error.URLError:
+            self.status_text = 'State: failed to send open'
             return False
 
         result = "success" in result.lower()
+        if result:
+            self.status_text = 'State: open successful'
+        else:
+            self.status_text = f'State: invalid passphrase: ' \
+                f'{self.config.get("Connection", "passphrase")}'
         return result
 
     @property
@@ -79,6 +136,7 @@ class GDOClient(App):
     def keep_open(self):
         return int(self.config.get('App Settings', 'keep'))
 
+    # noinspection PyUnusedLocal
     def keep_open_thread(self, dt):
         if self.keep_open:
             self.open_door()
@@ -91,13 +149,14 @@ class GDOClient(App):
             self.open_door()
         Clock.schedule_once(self.keep_open_thread, self.ival)
         Window.bind(on_keyboard=self.key_input)
+        self.status_text = 'State: unknown'
         return Interface()
 
     def build_config(self, config):
         config.setdefaults('Connection', {
             'https': 1,
             'address': 'gdo.tizilogic.com',
-            'port': 12321,
+            'port': 443,
             'passphrase': 'OpenUp'
         })
         config.setdefaults('App Settings', {
@@ -116,8 +175,10 @@ class GDOClient(App):
 
     # noinspection PyMethodMayBeStatic,PyUnusedLocal
     def key_input(self, window, key, scancode, codepoint, modifier):
-        if key == 27:
+        if key in (27, 4):
+            print('Back button pressed. Closing...')
             App.get_running_app().stop()
+            return True
         else:
             return False
 
